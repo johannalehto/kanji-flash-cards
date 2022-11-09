5ff0361ec9923d84d05d02ffc1bd166119842fa3
Johanna Lehto <johannaeveliinalehto@gmail.com>
2022-11-09 09:36:55 +0200

    Add Maksukortti-tests

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,34 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    # kortin saldo alussa oikein 
+    def test_kortin_saldo_alussa_oikein(self):
+        self.assertEqual(self.maksukortti.saldo, 1000)
+
+    # rahan lataaminen kasvattaa saldoa oikein
+    def test_rahan_lataaminen_kasvattaa_saldoa_oikein(self):
+        self.maksukortti.lataa_rahaa(500)
+        self.assertEqual(self.maksukortti.saldo, 1500)
+
+    ## rahan ottaminen toimii:
+
+    # saldo vähenee oikein, jos rahaa on tarpeeksi
+    def test_saldo_vahenee_oikein_jos_rahaa_tarpeeksi(self):
+        self.maksukortti.ota_rahaa(500)
+        self.assertEqual(self.maksukortti.saldo, 500)
+
+    # saldo ei muutu jos rahaa ei ole tarpeeksi
+    def test_saldo_ei_muutu_jos_rahaa_ei_tarpeeksi(self):
+        self.maksukortti.ota_rahaa(2000)
+        self.assertEqual(self.maksukortti.saldo, 1000)
+
+    # metodi palauttaa true, jos rahat riittivät, muutoin false
+    def test_rahat_riittivat(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(500), True)
+
+    def test_rahat_eivat_riita(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(1500), False)
+
+    def test_tulostus_oikein(self):
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
