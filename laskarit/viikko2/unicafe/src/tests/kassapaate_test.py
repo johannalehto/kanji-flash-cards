61b56a0f7fba16c5716088d668a520cdcd54e511
Johanna Lehto <johannaeveliinalehto@gmail.com>
2022-11-09 13:19:14 +0200

    Add edullisesti_kortilla and maukkaasti_kortilla -tests

--- laskarit/viikko2/unicafe/src/tests/kassapaate_test.py
+++ laskarit/viikko2/unicafe/src/tests/kassapaate_test.py
@@ -1,12 +1,14 @@
 import unittest
 from kassapaate import Kassapaate
+from maksukortti import Maksukortti
 
 class TestKassapaate(unittest.TestCase):
     def setUp(self):
         self.kassapaate = Kassapaate()
+        self.kortti_riittaa = Maksukortti(1000)
+        self.kortti_riittamaton = Maksukortti(100)
         
 # Luokan konstruktio oikein
-
     def test_luotu_kassapaate_kassassa_rahaa_oikein(self):
         self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
 
@@ -16,8 +18,7 @@ class TestKassapaate(unittest.TestCase):
     def test_luotu_kassapaate_maukkaat_oikein(self):
         self.assertEqual(self.kassapaate.maukkaat, 0)
 
-# Edulliset
-# Jos maksu riittävä: kassassa oleva rahamäärä kasvaa lounaan hinnalla ja vaihtorahan suuruus on oikea
+# Edulliset käteisellä
     def test_edullisesti_kateisella_riittava_maksu_kassassa_rahaa(self):
         self.kassapaate.syo_edullisesti_kateisella(500)
         self.assertEqual(self.kassapaate.kassassa_rahaa, 100240)
@@ -29,7 +30,6 @@ class TestKassapaate(unittest.TestCase):
     def test_edullisesti_kateisella_riittava_maksu_vaihtoraha_oikein(self):
         self.assertEqual(self.kassapaate.syo_edullisesti_kateisella(500), 260)
 
-# Maksu ei riittävä
     def test_edullisesti_kateisella_riittamaton_maksu_kassassa_rahaa(self):
         self.kassapaate.syo_edullisesti_kateisella(200)
         self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
@@ -42,9 +42,7 @@ class TestKassapaate(unittest.TestCase):
         self.kassapaate.syo_edullisesti_kateisella(200)
         self.assertEqual(self.kassapaate.syo_edullisesti_kateisella(200), 200)
 
-
-# Maukkaat
-# Jos maksu riittävä: kassassa oleva rahamäärä kasvaa lounaan hinnalla ja vaihtorahan suuruus on oikea
+# Maukkaat käteisellä
     def test_maukkaasti_kateisella_riittava_maksu_kassassa_rahaa(self):
         self.kassapaate.syo_maukkaasti_kateisella(500)
         self.assertEqual(self.kassapaate.kassassa_rahaa, 100400)
@@ -56,7 +54,6 @@ class TestKassapaate(unittest.TestCase):
     def test_maukkaasti_kateisella_riittava_maksu_vaihtoraha_oikein(self):
         self.assertEqual(self.kassapaate.syo_maukkaasti_kateisella(500), 100)
 
-# Maksu ei riittävä
     def test_maukkaasti_kateisella_riittamaton_maksu_kassassa_rahaa(self):
         self.kassapaate.syo_maukkaasti_kateisella(200)
         self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
@@ -69,20 +66,63 @@ class TestKassapaate(unittest.TestCase):
         self.kassapaate.syo_maukkaasti_kateisella(200)
         self.assertEqual(self.kassapaate.syo_maukkaasti_kateisella(200), 200)
 
+#Edulliset kortilla
+    def test_edullisesti_kortilla_riittava_maksu_edulliset_kasvaa(self):
+        self.kassapaate.syo_edullisesti_kortilla(self.kortti_riittaa)
+        self.assertEqual(self.kassapaate.edulliset, 1)
+
+    def test_edullisesti_kortilla_riittava_maksu_summa_veloitetaan_kortilta(self):
+        self.kassapaate.syo_edullisesti_kortilla(self.kortti_riittaa)
+        self.assertEqual(self.kortti_riittaa.saldo, 760)
+
+    def test_edullisesti_kortilla_riittava_maksu_onnistui(self):
+        self.assertEqual(self.kassapaate.syo_edullisesti_kortilla(self.kortti_riittaa), True)
+
+    def test_edullisesti_kortilla_riittamaton_maksu_edulliset_ei_kasva(self):
+        self.kassapaate.syo_edullisesti_kortilla(self.kortti_riittamaton)
+        self.assertEqual(self.kassapaate.edulliset, 0)
+
+    def test_edullisesti_kortilla_riittamaton_maksu_summa_veloitetaan_kortilta(self):
+        self.kassapaate.syo_edullisesti_kortilla(self.kortti_riittamaton)
+        self.assertEqual(self.kortti_riittamaton.saldo, 100)
+
+    def test_edullisesti_kortilla_riittamaton_maksu_epaonnistui(self):
+        self.assertEqual(self.kassapaate.syo_edullisesti_kortilla(self.kortti_riittamaton), False)
+
+#Maukkaat kortilla
+    def test_maukkaasti_kortilla_riittava_maksu_maukkaat_kasvaa(self):
+        self.kassapaate.syo_maukkaasti_kortilla(self.kortti_riittaa)
+        self.assertEqual(self.kassapaate.maukkaat, 1)
+
+    def test_maukkaasti_kortilla_riittava_maksu_summa_veloitetaan_kortilta(self):
+        self.kassapaate.syo_maukkaasti_kortilla(self.kortti_riittaa)
+        self.assertEqual(self.kortti_riittaa.saldo, 600)
+
+    def test_maukkaasti_kortilla_riittava_maksu_onnistui(self):
+        self.assertEqual(self.kassapaate.syo_maukkaasti_kortilla(self.kortti_riittaa), True)
+
+    def test_maukkaasti_kortilla_riittamaton_maksu_maukkaat_ei_kasva(self):
+        self.kassapaate.syo_maukkaasti_kortilla(self.kortti_riittamaton)
+        self.assertEqual(self.kassapaate.maukkaat, 0)
+
+    def test_maukkaasti_kortilla_riittamaton_maksu_summa_veloitetaan_kortilta(self):
+        self.kassapaate.syo_maukkaasti_kortilla(self.kortti_riittamaton)
+        self.assertEqual(self.kortti_riittamaton.saldo, 100)
+
+    def test_maukkaasti_kortilla_riittamaton_maksu_epaonnistui(self):
+        self.assertEqual(self.kassapaate.syo_maukkaasti_kortilla(self.kortti_riittamaton), False)
+
+
+    def test_edullisesti_kortilla_kassa_ei_muutu(self):
+        self.kassapaate.syo_edullisesti_kortilla(self.kortti_riittaa)
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
+
+    def test_maukkaasti_kortilla_kassa_ei_muutu(self):
+        self.kassapaate.syo_maukkaasti_kortilla(self.kortti_riittaa)
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
+        
 """
 
-
-# Käteisosto toimii sekä edullisten että maukkaiden lounaiden osalta
-
-
-#Jos maksu on riittävä: myytyjen lounaiden määrä kasvaa
-#Jos maksu ei ole riittävä: kassassa oleva rahamäärä ei muutu, kaikki rahat palautetaan vaihtorahana ja myytyjen lounaiden määrässä ei muutosta
-
-seuraavissa testeissä tarvitaan myös Maksukorttia jonka oletetaan toimivan oikein
-# Korttiosto toimii sekä edullisten että maukkaiden lounaiden osalta
-# Jos kortilla on tarpeeksi rahaa, veloitetaan summa kortilta ja palautetaan True
-# Jos kortilla on tarpeeksi rahaa, myytyjen lounaiden määrä kasvaa
-# Jos kortilla ei ole tarpeeksi rahaa, kortin rahamäärä ei muutu, myytyjen lounaiden määrä muuttumaton ja palautetaan False
 # Kassassa oleva rahamäärä ei muutu kortilla ostettaessa
 # Kortille rahaa ladattaessa kortin saldo muuttuu ja kassassa oleva rahamäärä kasvaa ladatulla summalla
 
