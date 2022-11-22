f0f2f0a2c17b592ee57d0445d1164ac01fc9d2ef
Johanna Lehto <johannaeveliinalehto@gmail.com>
2022-11-22 18:07:24 +0200

    Add a simple point counter

--- kanji-app/src/review.py
+++ kanji-app/src/review.py
@@ -1,6 +1,7 @@
 class Review:
     def __init__(self, kanjiset):
         self.kanjiset = kanjiset
+        self.session_points = 0
         
     def review_cards(self):
         print("")
@@ -11,16 +12,24 @@ class Review:
 
 
         for card in self.kanjiset:
-            print("-------------------------------------")
+            print("-----------------------------------")
             print("")
             print(f'KANJI: {card["kanji"]}')
             answer = input("MEANING: ")
             if answer == card["english"]:
                 print(f'Correct!')
+                self.session_points += 1
             else:
-                print(f'Wrong. The right answer is:: {card["english"]}')
+                print(f'Wrong. The right answer is:  {card["english"]}')
             command = input("")
             if command== "":
                 continue
-        print("-------------------------------------")
+        print("-----------------------------------")
+        print("")
         print("End of the set")
+        print(f'You got {self.session_points} / {len(self.kanjiset)} correct')
+        print("")
+        command = input("Press enter to return to the main menu")
+        print("")
+        if command == "":
+            return
