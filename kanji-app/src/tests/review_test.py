b89b158a17198fb2d05ee989a4f2764613c4fd7f
Johanna Lehto <johannaeveliinalehto@gmail.com>
2022-11-22 19:13:00 +0200

    Add test for Check meaning: check correct answer

--- kanji-app/src/tests/review_test.py
+++ kanji-app/src/tests/review_test.py
@@ -1,8 +1,25 @@
 import unittest
 from review import Review
 
+class KanjisetStub:
+    def get_kanjilist(self):
+        return [
+        {
+        "kanji": "山",
+        "english": "mountain"
+        },
+        {
+        "kanji": "木",
+        "english": "tree"
+        }
+        ]
+
 class TestReview(unittest.TestCase):
     def setUp(self):
-        print("Set up goes here")
+        self.kanjiset = Review(KanjisetStub())
 
-    
+    def test_check_cards_check_meaning_correct_answer(self):
+        card_answer = "mountain"
+        user_answer = "mountain"
+        correct_answer = self.kanjiset.check_meaning(user_answer, card_answer)
+        self.assertEqual(correct_answer, "Correct!")
