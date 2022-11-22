099b94cd75baa58182ef201394c5140aef0c5c1d
Johanna Lehto <johannaeveliinalehto@gmail.com>
2022-11-22 18:19:33 +0200

    Restructurise to separate methods

--- kanji-app/src/review.py
+++ kanji-app/src/review.py
@@ -2,15 +2,9 @@ class Review:
     def __init__(self, kanjiset):
         self.kanjiset = kanjiset
         self.session_points = 0
-        
-    def review_cards(self):
-        print("")
-        print("###########################################")
-        print("")
-        print("Showing a set of 5 kanji")
-        print("Write the meaning in English and press enter to move on to a next card")
 
 
+    def place_cards(self):
         for card in self.kanjiset:
             print("-----------------------------------")
             print("")
@@ -24,12 +18,34 @@ class Review:
             command = input("")
             if command== "":
                 continue
-        print("-----------------------------------")
-        print("")
-        print("End of the set")
+    
+    def show_points(self):
         print(f'You got {self.session_points} / {len(self.kanjiset)} correct')
+
+    def return_to_main_menu(self):
         print("")
         command = input("Press enter to return to the main menu")
         print("")
         if command == "":
             return
+
+
+ 
+    def review_cards(self):
+        print("")
+        print("###########################################")
+        print("")
+        print("Showing a set of 5 kanji")
+        print("Write the meaning in English and press enter to move on to a next card")
+
+        self.place_cards()
+
+        print("-----------------------------------")
+        print("")
+        print("End of the set")
+
+        self.show_points()
+
+        self.return_to_main_menu()
+
+
