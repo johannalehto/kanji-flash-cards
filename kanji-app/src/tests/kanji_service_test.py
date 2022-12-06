5450412d8185e1f8de738f7a438df3e43eadb7ff
Johanna Lehto <johannaeveliinalehto@gmail.com>
2022-12-06 20:38:54 +0200

    Update tests

---
+++ kanji-app/src/tests/kanji_service_test.py
@@ -0,0 +1,11 @@
+import unittest
+from services.kanji_service import KanjiService
+
+
+class TestKanjiService(unittest.TestCase):
+    def setUp(self):
+        self.service = KanjiService()
+
+    
+
+    
