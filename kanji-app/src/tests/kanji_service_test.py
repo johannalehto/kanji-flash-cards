a3a0dec0b0fd7781bbcb74886e63b986bef9e5ab
Johanna Lehto <johannaeveliinalehto@gmail.com>
2022-12-06 21:17:20 +0200

    Add tests for check_meaning-method

--- kanji-app/src/tests/kanji_service_test.py
+++ kanji-app/src/tests/kanji_service_test.py
@@ -1,11 +1,28 @@
 import unittest
 from services.kanji_service import KanjiService
+from entities.card import Card
+from entities.kanji import Kanji
+
+class CardStub:
+    def __init__(self):
+        self.kanji = Kanji("山", "mountain")
+
+    def meaning(self):
+        return self.kanji.english
+    
 
 
 class TestKanjiService(unittest.TestCase):
     def setUp(self):
         self.service = KanjiService()
 
-    
+    def test_check_meaning_returns_true_when_answer_correct(self):
+        card = CardStub()
+        self.assertTrue(self.service.check_meaning("mountain", card))
+
+    def test_check_meaning_returns_false_when_answer_incorrect(self):
+        card = CardStub()
+        self.assertFalse(self.service.check_meaning("yama", card))
+
 
     
