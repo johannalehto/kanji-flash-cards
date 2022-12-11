7aa06d1b183d9145a6d77111ef633412837b406d
Johanna Lehto <johannaeveliinalehto@gmail.com>
2022-12-11 22:54:09 +0200

    Update ui to gui

--- kanji-app/src/index.py
+++ kanji-app/src/index.py
@@ -1,10 +1,22 @@
-from ui.ui import UI
+from tkinter import Tk
+#from ui.ui import UI
 from ui.gui import GUI
 
+BACKGROUND_COLOR = "#fff"
 
 def main():
-    kanjiapp = GUI()
-    kanjiapp.run()
+    window = Tk()
+    window.title("Kanji Flashcards App")
+    window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+    kanjiapp = GUI(window)
+    kanjiapp.start()
+
+    window.mainloop()
+
+# def main():
+#     kanjiapp = GUI()
+#     kanjiapp.run()
 
 
 if __name__ == "__main__":
