0c553d925f32b31a0f828840acd9cb1e568e1a34
Johanna Lehto <johannaeveliinalehto@gmail.com>
2022-12-11 19:55:36 +0200

    Add canvas, add character-component and meaning entry

--- kanji-app/src/ui/gui.py
+++ kanji-app/src/ui/gui.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, ttk, Canvas
+from tkinter import *
 
 BACKGROUND_COLOR = "#fff"
 
@@ -6,15 +6,22 @@ class GUI:
     def __init__(self, root):
         self._root = root
 
-    def start(self):
-        label = ttk.Label(master=self._root, text="Hello world!")
+    def start(self):        
+        canvas = Canvas(self._root, width=800, height=400)
+        canvas.pack()
+        card_character = canvas.create_text(400, 150, text="漢字", font=("Arial", 80, "bold"))
+        card_meaning_title = canvas.create_text(400, 250, text="meaning:", font=("Arial", 24))
 
-        label.pack()
+        card_meaning_entry = Entry(master=self._root, font=("Arial", 24), width=34, fg="#000")
+        entry_window = canvas.create_window(400, 300, window=card_meaning_entry)
+
+        canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+        canvas.grid(row=0, column=0, columnspan=2)
+
 
 window = Tk()
 window.title("Kanji Flashcards App")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
-
 
 
 ui = GUI(window)
