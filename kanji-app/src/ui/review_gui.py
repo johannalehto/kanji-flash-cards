3a54efb32f9a14daaf8cf4b7533bb846c2b060cc
Johanna Lehto <johannaeveliinalehto@gmail.com>
2022-12-22 17:22:41 +0200

    Add methods to run the cards after another

--- kanji-app/src/ui/review_gui.py
+++ kanji-app/src/ui/review_gui.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from services.kanji_service import KanjiService
+import random
 
 BACKGROUND_COLOR = "#fff"
 word_file = "./src/data/default.csv"
@@ -42,25 +43,55 @@ class ReviewGUI:
         self._canvas = None
         self._character = None
         self._meaning_entry = None
+        self._next_button = None
         self._card = None
+        self._viewed  = []
 
         self._initialize()
+        self._run_cards()
 
     def pack(self):
         """"Displays the view"""
         self._canvas.pack()
 
-    # def destroy(self):
-    #     """"Destroys the view"""
-    #     self._canvas.delete('all')
+    def destroy(self):
+        """"Destroys the view"""
+        self._canvas.delete('all')
 
     def _return_handler(self):
         """"Responsible for returning to the menu view"""
         self._handle_return()
 
-    def _handle_answer(self):
+
+    def _initialize(self):
+        """"Initializes the card round view"""
+        self._canvas = Canvas(self._root, width=800, height=526)
+        self._canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+        self._canvas.grid(row=0, column=0, columnspan=2)
+  
+
+    def _run_cards(self):
+
+        self._initialize_card()
+
+        self._card = self._give_a_card()
+        self._canvas.itemconfig(self._character, text=self._card.character(), fill="black")
+        self._meaning_entry.bind('<Return>', self._handle_answer)
+        # self._meaning_entry.bind('<Return>', self._clear, add="+")  
+
+
+    def _give_a_card(self):
+        # card = random.choice(self._pile)
+        # if card not in self._viewed:
+        #     self._viewed.append(card)
+        #     return card
+        return random.choice(self._pile)
+        
+
+    def _handle_answer(self, event):
         """"Checks whether answer is correct from the service"""
-        answer = self._card_meaning_entry.get()
+        answer = self._meaning_entry.get()
+        print(answer)
         if self._service.check_meaning(answer, self._card):
             self._result("Correct")
         else:
@@ -72,26 +103,9 @@ class ReviewGUI:
             text=result, 
             font=("Arial", 24))
 
-
-    def _initialize(self):
-        """"Initializes the card round view"""
-        self._canvas = Canvas(self._root, width=800, height=526)
-        self._canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
-        self._canvas.grid(row=0, column=0, columnspan=2)
-  
-        self._initialize_card()
-        self._initialize_entry()
-
-        for card in self._pile:
-            self._card = card
-            self._canvas.itemconfig(self._character, text=self._card.character(), fill="black")
-           # self._meaning_entry.bind('<Return>', self._handle_answer())   
-            breakpoint()
-            self._handle_next()
-    
-
     def _handle_next(self):
-        pass
+        self.destroy()
+        self._run_cards()
         
 
     def _initialize_card(self):
@@ -100,37 +114,33 @@ class ReviewGUI:
             text="漢字", 
             font=("Arial", 80, "bold"))
 
-    
-    def _initialize_entry(self):
-        _meaning_title = self._canvas.create_text(
+        meaning_title = self._canvas.create_text(
             400, 250, 
             text="meaning:", 
             font=("Arial", 24))
 
-        _meaning_entry = Entry(
+        self._meaning_entry = Entry(
             self._root,
             font=("Arial", 24), 
             width=34, 
             fg="#000")
 
-        
-        # _button = Button(
-        #   master=self._frame,
-        #   text="Check",
-        #   command=self._handle_answer()
-        # )
-
-       # _meaning_entry.grid(row=1, column=1)
-
-        _entry_window = self._canvas.create_window(
+        entry_window = self._canvas.create_window(
             400, 300, 
-            window=_meaning_entry)
+            window=self._meaning_entry)
 
- 
+        next_button = Button(
+          self._root,
+          text="Next",
+          command=self._handle_next
+        )
 
-        # _button_window = self._canvas.create_window(
-        #     400, 330, 
-        #     window=_button)
+        button_window = self._canvas.create_window(
+            400, 330, 
+            window=next_button)
+
+
+
 
 
 
