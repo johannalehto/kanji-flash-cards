8094a6ae99598072728d556e0f66410db5ebfa0d
Johanna Lehto <johannaeveliinalehto@gmail.com>
2022-12-25 23:22:40 +0200

    Update, remove excess

--- kanji-app/src/tests/entities/card_test.py
+++ kanji-app/src/tests/entities/card_test.py
@@ -1,16 +1,9 @@
 import unittest
 from entities.card import Card
-from entities.card import Kanji
-from services.kanji_service import KanjiService
-
-
-
 
 class TestCard(unittest.TestCase):
     def setUp(self):
-        #self.kanji = Kanji("山", "mountain")
         self.card = Card("山", "mountain")
-        #self.service = KanjiService()
 
     def test_meaning_call(self):
         self.assertEqual(self.card.meaning(), "mountain")
